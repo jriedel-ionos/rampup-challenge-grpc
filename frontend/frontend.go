316bf76b2cb6fdaed7c3e4a0b98c839c0ad44a4d
Jonas Riedel <138458199+jriedel-ionos@users.noreply.github.com>
2023-07-05 15:36:12 +0200

    feat: implement frontend (#1)
    * feat: frontend displaying env variable given in url
    
    * refactor: rename server file
    
    * docs: add more information about usage
    
    * fix: ensure template file is existing
    
    * feat: golangci-lint config
    
    * fix: embed template file
    
    * refactor: rename linter config
    
    * refactor: applied linter feedback
    
    * refactor: fix imports
    
    * feat: make port configurable
    
    * fix: dont parse template on every request
    
    * feat: configure port via cli flag
    
    * fix: change port to int
    
    * docs: use double hyphen
    
    * refactor(frontend): use Sprintf instead of strconv
    
    * refactor(server): use Sprintf instead of strconv
    
    * fix(server): imports lint
    
    * refactor(frontend): extend error handling
    
    * feat(frontend): implement actual gRPC request to backend
    
    * refactor(server): use log instead of fmt
    
    * refactor(frontend): Use mainImpl pattern in main function
    
    * refactor(frontend): remove wrong logs

---
+++ frontend/frontend.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/jriedel-ionos/rampup-challenge-grpc/server/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type PageData struct {
+	Value string
+}
+
+//go:embed index.html
+var templateFile embed.FS
+
+func main() {
+	if err := mainImpl(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mainImpl() error {
+	port := flag.Int("port", 8081, "port for the frontend")
+	backendAddress := flag.String("backend", "localhost:8080", "address of the backend server")
+	flag.Parse()
+
+	tmpl, err := template.ParseFS(templateFile, "index.html")
+	if err != nil {
+		return fmt.Errorf("failed to parse template: %v", err)
+	}
+
+	conn, err := grpc.Dial(*backendAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("failed to connect to backend server: %v", err)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Failed to close gRPC connection: %v", err)
+		}
+	}()
+
+	client := pb.NewEnvVariableClient(conn)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		varName := r.URL.Path[1:]
+
+		resp, err := client.GetEnvironmentVariable(context.Background(), &pb.GetEnvironmentVariableRequest{
+			VariableName: varName,
+		})
+		if err != nil {
+			log.Printf("Failed to get environment variable: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		data := PageData{Value: resp.Value}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			log.Printf("Failed to render template: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Printf("Frontend server started on port %v", *port)
+	err = http.ListenAndServe(fmt.Sprintf(":%v", *port), nil)
+	if err != nil {
+		return fmt.Errorf("failed to start frontend server: %v", err)
+	}
+
+	return nil
+}
---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/jriedel-ionos/rampup-challenge-grpc/server/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedEnvVariableServer
+}
+
+func (s *server) GetEnvironmentVariable(
+	_ context.Context,
+	in *pb.GetEnvironmentVariableRequest,
+) (
+	*pb.GetEnvironmentVariableResponse,
+	error,
+) {
+	value := os.Getenv(in.VariableName)
+	log.Println(value)
+	return &pb.GetEnvironmentVariableResponse{
+		Value: value,
+	}, nil
+}
+
+func main() {
+	port := flag.Int("port", 8080, "port for the backend")
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Backend server started on port %v", *port)
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterEnvVariableServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
